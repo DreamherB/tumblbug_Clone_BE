61546d9277cf374eb25306819a57d11d260e69fb
Hoon333 <wkdzhd2000@gmail.com>
2022-02-21 23:10:23 +0900

    original auth

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../schemas/user');
 const { JWT_SECRET_KEY } = process.env;
 require('dotenv').config();
-const bcrypt = require("bcrypt")
+
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -29,25 +29,4 @@ module.exports = (req, res, next) => {
     });
     return;
   }
-};
-
-
-// 사전 hook , save 시 password 암호화 해서 저장
-UserSchema.pre("save", function (next) {
-  let user = this;
-  bcrypt.genSalt(10, (err, salt) => {
-      if (err) return next(err);
-      bcrypt.hash(user.password, salt, (err, hash) => {
-          if (err) return next(err);
-          user.password = hash;
-          next();
-      })
-  })
-})
-
-
-// 해당 메소드 사용 시 기존과 비교 실시
-UserSchema.methods.compare = function (password) {
-  let user = this;
-  return bcrypt.compareSync(password, user.password);
 };
