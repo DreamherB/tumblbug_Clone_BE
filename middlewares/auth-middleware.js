4c90ce0cbc35769b211947c931097c96dabef9f8
Hoon333 <wkdzhd2000@gmail.com>
2022-02-23 19:17:31 +0900

    kakao pw not needed during kakao login

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,38 +1,35 @@
-const jwt = require('jsonwebtoken');
-const User = require('../schemas/user');
-require('dotenv').config();
-const { JWT_SECRET_KEY } = process.env
-
-
+const jwt = require("jsonwebtoken");
+const User = require("../schemas/user");
+require("dotenv").config();
+const { JWT_SECRET_KEY } = process.env;
 
 module.exports = (req, res, next) => {
-  
-  const {authorization} = req.headers; 
-  
-  const [tokenType, tokenValue] = authorization.split(' ');
-  console.log(tokenType)
-  console.log(tokenValue)
+    const { authorization } = req.headers;
 
-  if (tokenType !== 'Bearer') {
-    res.status(401).send({
-      errorMessage: '로그인 후 사용하세요.',
-    });
-    
-    return;
-  }
+    const [tokenType, tokenValue] = authorization.split(" ");
+    console.log(tokenType);
+    console.log(tokenValue);
 
-  try {
-    const { email } = jwt.verify(tokenValue, JWT_SECRET_KEY);
-    User.findOne({ email })
-      .exec()
-      .then((user) => {
-        res.locals.user = user;
-        next();
-      });
-  } catch (error) {
-    res.status(401).send({
-      errorMessage: '로그인 후 사용하세요.',
-    });
-    return;
-  }
+    if (tokenType !== "Bearer") {
+        res.status(401).send({
+            errorMessage: "로그인 후 사용하세요.",
+        });
+
+        return;
+    }
+
+    try {
+        const { email } = jwt.verify(tokenValue, JWT_SECRET_KEY);
+        User.findOne({ email })
+            .exec()
+            .then((user) => {
+                res.locals.user = user;
+                next();
+            });
+    } catch (error) {
+        res.status(401).send({
+            errorMessage: "로그인 후 사용하세요.",
+        });
+        return;
+    }
 };
--- schemas/user.js
+++ schemas/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
-
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
     nickname: {
@@ -16,20 +15,34 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-
 // 사전 hook , save 시 password 암호화 해서 저장
 UserSchema.pre("save", function (next) {
     let user = this;
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) return next(err);
-            user.password = hash;
-            next();
-        })
-    })
-})
 
+    if (user.password) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err);
+                user.password = hash;
+            });
+        });
+    }
+    next();
+});
+
+// 이전의 코드, 카카오 로그인 시 password라는 data가 없기 때문에 Error: data and salt arguments required 가 발생한다.
+// UserSchema.pre("save", function (next) {
+//     let user = this;
+//     bcrypt.genSalt(10, (err, salt) => {
+//         if (err) return next(err);
+//         bcrypt.hash(user.password, salt, (err, hash) => {
+//             if (err) return next(err);
+//             user.password = hash;
+//             next();
+//         });
+//     });
+// });
 
 // 해당 메소드 사용 시 기존과 비교 실시
 UserSchema.methods.compare = function (password) {
