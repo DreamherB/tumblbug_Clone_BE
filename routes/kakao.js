8726a67c8e03b8cc369357798f8c3e9503ea8f61
Hoon333 <wkdzhd2000@gmail.com>
2022-02-24 16:15:53 +0900

    kakaoLogin edited: duplicate email check

--- routes/kakao.js
+++ routes/kakao.js
@@ -8,9 +8,17 @@ const { JWT_SECRET_KEY } = process.env;
 router.post("/users/loginKakao", async (req, res) => {
     const { email, nickname } = req.body;
 
+    const existsUsers = await User.findOne({ email });
+    if (existsUsers) {
+        // NOTE: 보안을 위해 인증 메세지는 자세히 설명하지 않는것을 원칙으로 한다: https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#authentication-responses
+        return res.status(400).send({
+            msg: "이미 존재하는 이메일입니다.",
+        });
+    }
+
     const user = new User({ email, nickname });
     await user.save();
-    
+
     res.send({
         result: true,
         token: jwt.sign({ email: user.email }, JWT_SECRET_KEY),
